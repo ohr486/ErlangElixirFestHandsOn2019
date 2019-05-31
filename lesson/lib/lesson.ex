4f1ddfe4d4a5d5f43081f3e7ff0cbb76dcb59219
ohr486 <ohr486@gmail.com>
2019-06-01 04:59:30 +0900

    update exec_xxx

--- lesson/lib/lesson.ex
+++ lesson/lib/lesson.ex
@@ -27,25 +27,25 @@ defmodule Lesson do
   def sleep_rand(n) do
     sleep_time = :rand.uniform(n)
     :timer.sleep(sleep_time)
-    IO.puts "#{sleep_time}ミリ秒sleepしました"
+    #IO.puts "#{sleep_time}ミリ秒sleepしました"
     sleep_time
   end 
 
-  # n回、sleep_randを繰り返す 
+  # 6-4
+  # n回、sleep_rand(1000)を繰り返す 
   def exec_seq(n) do
     IO.puts "=== 開始: #{n}回rand_sleepを繰り返す ==="
-    result = 1 .. n
-             |> Enum.map(fn(_) -> sleep_rand(1000) end)
+    result = Enum.map(1 .. n, fn(_) -> sleep_rand(1000) end)
     IO.puts "=== 終了: #{n}回rand_sleepを繰り返す ==="
     result
   end 
 
-  # n回、sleep_randを並列に実行する
+  # 7-4
+  # n回、sleep_rand(1000)を並列に実行する
   def exec_pal(n) do
     IO.puts "=== 開始: #{n}回rand_sleepを繰り返す(並列) ==="
-    result = 1 .. n
-             |> Enum.map(fn(_) -> Task.async(Lesson, :sleep_rand, [1000]) end)
-             |> Enum.map(fn(d) -> Task.await(d) end)
+    list = Enum.map(1..n, fn(_) -> Task.async(Lesson, :sleep_rand, [1000]) end)
+    result = Enum.map(list, fn(d) -> Task.await(d) end)
     IO.puts "=== 終了: #{n}回rand_sleepを繰り返す(並列) ==="
     result
   end
